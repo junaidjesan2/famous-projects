6121d78e3a6c671680154f0960964f282c55fa0b
junaidjesan2 <junaidjesan2@gmail.com>
2024-02-21 20:48:50 +0600

    random color generator has created, both rgb and hex

--- src/components/Pages/rendomColor/rendomColor.jsx
+++ src/components/Pages/rendomColor/rendomColor.jsx
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 export default function RendomColor() {
   const [colorType, setColorType] = useState("hex");
-  const [randomColor,SetRandomColor]=useState("#000000");
+  const [randomColor, SetRandomColor] = useState("#0000000");
 
-  function generateHex() {}
-  function generateRbg() {}
+  function HEX(number) {
+    return Math.floor(Math.random() * number);
+  }
+
+  function generateHex() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[HEX(hex.length)];
+    }
+    SetRandomColor(hexColor);
+  }
+  function generateRbg() {
+    const R = Math.round(Math.random() * 255);
+    const G = Math.round(Math.random() * 255);
+    const B = Math.round(Math.random() * 255);
+    SetRandomColor(`rgb(${R},${G},${B})`);
+  }
   return (
     <div className="h-screen py-5" style={{ background: randomColor }}>
       <div className="w-2/4 mx-auto">
@@ -28,6 +44,7 @@ export default function RendomColor() {
           Generate random color
         </button>
       </div>
+      <div className="text-6xl">{randomColor}</div>
     </div>
   );
 }
